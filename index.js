4dcea3c773553c8026aa22854f26d4556bd5d6e8
Meliana1020 · <melianaa1020@gmail.com>
2026-05-01 10:24:52 +0700

    update api inventory

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/analysis', analysisRoutes);
-app.use('/api/inventory', inventoryRoutes);
+app.use('/api/inventory/daily', inventoryRoutes);
 
 app.get('/', (req, res) => {
     res.send('API SPK Toko LM Baedors Mart is Running! 🚀');
