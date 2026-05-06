746b38e4158445cc9743e1f3cdb9d60f685e7d11
Meliana1020 · <melianaa1020@gmail.com>
2026-05-06 10:47:20 +0700

    rename auto-complete-list

--- src/components/fragments/dashboard/inventory/form.tsx
+++ src/components/fragments/dashboard/inventory/form.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import InventoryFormHeader from "./header";
-import AutocompleteList from "./auto-complate-list";
+import AutocompleteList from "./auto-complete-list";
 
 interface InventoryFormProps {
   isEditing: boolean;
